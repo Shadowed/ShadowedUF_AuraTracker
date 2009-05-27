97111c3e0712c1639ddd1450b6b41b8731bf5754
Proditor <alex@proditor.net>
2009-05-27 19:48:36 +0200

    * Updated so it doesn't use AceAddon-3.0.

--- ShadowedUF_AuraTracker.lua
+++ ShadowedUF_AuraTracker.lua
@@ -1,13 +1,20 @@
 local ShadowUF = ShadowUF
-local AuraTracker = ShadowUF:NewModule("AuraTracker")
+local AuraTracker = {}
 ShadowUF:RegisterModule(AuraTracker, "AuraTracker", "AuraTracker")
 
 function AuraTracker:OnInitialize()
-	for _, unit in pairs(ShadowUF.units) do
-		if ( ShadowUF.db.profile.units[unit].AuraTracker == nil ) then
-			ShadowUF.db.profile.units[unit].AuraTracker = { enabled = true }
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("ADDON_LOADED")
+	frame:SetScript("OnEvent", function(self, event, addon)
+		if( IsAddOnLoaded("ShadowedUF_AuraTracker") ) then
+			self:UnregisterEvent("ADDON_LOADED")
+			for _, unit in pairs(ShadowUF.units) do
+				if ( ShadowUF.db.profile.units[unit].AuraTracker == nil ) then
+					ShadowUF.db.profile.units[unit].AuraTracker = { enabled = true }
+				end
+			end
 		end
-	end
+	end)
 end
 
 function AuraTracker:UnitEnabled(frame, unit)
